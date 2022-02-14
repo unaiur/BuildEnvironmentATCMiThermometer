a5393d53809cfe44723e5986208e0a1fef0f7801
Unai Uribarri <unaiur@gmail.com>
2022-02-14 12:10:02 +0100

    Modify it to support building my own version of the firmware

--- 00_bootstrap.sh
+++ 00_bootstrap.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 echo \#\# First prepare build environment
-curl  http://shyboy.oss-cn-shenzhen.aliyuncs.com/readonly/tc32_gcc_v2.0.tar.bz2 > docker/tc32_gcc.tar.bz2
-docker build ./docker/ -t tc32
-
-
-echo \#\# Now download Telink 825X SDK
-git clone --depth=1 https://github.com/Ai-Thinker-Open/Telink_825X_SDK SDK
-
+docker build ./docker/ -t unaiur-atc-mithermometer
 
 echo \#\# Download ATC_MiThermometer project
-git clone https://github.com/atc1441/ATC_MiThermometer
+git clone https://github.com/unaiur/ATC_MiThermometer
